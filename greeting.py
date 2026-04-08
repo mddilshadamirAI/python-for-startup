2e814bdfa6d692d4f010a83172f19189e0e5be16
mddilshadamir <mddilshadamir@gmail.com>
2026-04-08 06:47:50 +0000

    the 2nd code of day 2

---
+++ greeting.py
@@ -0,0 +1,9 @@
+def greet(input):
+    if "hello" in input:
+        return "hello, there "
+    else:
+        return "i am not sure what you mean"
+
+
+greeting = greet("hello computer")
+print(greeting + "Dilshad")
