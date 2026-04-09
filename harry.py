fc9873fa1bf24872c615b55a3818e6acf120c7c7
mddilshadamir <mddilshadamir@gmail.com>
2026-04-09 17:07:18 +0000

    This is code of harry houses

---
+++ harry.py
@@ -0,0 +1,28 @@
+name = input("What's your name? ")
+
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
+
+
+name = input("What's your name? ")
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
