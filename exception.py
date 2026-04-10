13a0853b171c76e89d93411aaf98ee9069d74fdf
mddilshadamir <mddilshadamir@gmail.com>
2026-04-10 12:39:37 +0000

    exception understanding

---
+++ exception.py
@@ -0,0 +1,43 @@
+x = int(input("What's x?"))
+print(f"x is {x}")
+
+
+try:
+    x = int(input("What's x?"))
+    print(f"x is {x}")
+except ValueError:
+    print("x is not an integer")
+
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x?"))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+    return x
+
+
+main()
+
+
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
+
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+
+main()
