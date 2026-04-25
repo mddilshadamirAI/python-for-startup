16a8d3814b0ded7a5047e7f858d8e2c45f967394
mddilshadamir <mddilshadamir@gmail.com>
2026-04-25 13:57:59 +0000

    simple comparison

--- learning/compare.py
+++ learning/compare.py
@@ -1,7 +1,8 @@
-x = int(input("what's x?"))
-y = int(input("what's y?"))
+while True:
+    x = int(input("what's x? "))
+    y = int(input("what's y? "))
 
-if x == y:
-    print("x is equal to y")
-else:
-    print("x is not equal to y") 
+    if x == y:
+        print("x is equal to y")
+    else:
+        print("x is not equal to y")
